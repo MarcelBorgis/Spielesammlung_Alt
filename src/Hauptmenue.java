7c48e10e7d2bd8b722877aada79c0be6432d30e8
Marcel <marcel.borgis@msg-david.de>
2019-10-08 08:52:37 +0200

    Button und Label hinzugefügt

--- src/Hauptmenue.java
+++ src/Hauptmenue.java
@@ -1,29 +1,52 @@
 
 import javax.swing.*;
 import java.awt.*;
-
+import java.util.concurrent.Flow;
 
 
 public class Hauptmenue {
 
     public void hauptmenue() {
 
+        TikTakToeListener tListener = new TikTakToeListener();
+
         JFrame frame = new JFrame();
+        JPanel mainPanel = new JPanel();
         JButton button1 = new JButton();
+        Container container = new Container();
 
-        ImageIcon image = new ImageIcon(getClass().getResource("Unbennant.png"));
+        ImageIcon image = new ImageIcon("Unbenannt.png");
         JLabel label = new JLabel(image);
+        JButton spiel1 = new JButton();
 
 
-        frame.setSize(500, 500);
+        spiel1.setText("Tik Tak Toe");
+        spiel1.setForeground(Color.white);
+        spiel1.setBackground(Color.black);
+        spiel1.setFont(new Font("Arial", Font.PLAIN, 40));
+        spiel1.setBounds(200,200,10,10);
+        spiel1.setVisible(true);
+        spiel1.addActionListener(tListener);
+
+        label.setText("Hauptmenue");
+        label.setBounds(200,0,100,100);
+        label.setFont(new Font("Arial", Font.PLAIN, 20));
+        label.setVisible(true);
+
+        mainPanel.setVisible(true);
+        mainPanel.setBounds(0,0,500,500);
+        mainPanel.setBackground(Color.yellow);
+        mainPanel.add(label);
+        mainPanel.add(spiel1);
+
         frame.setTitle("Hauptmenü");
         frame.setBackground(Color.red);
-
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(500, 500, 500, 500);
         frame.setResizable(false);
         frame.setVisible(true);
+        frame.add(mainPanel);
+
     }
 
 
--- src/TikTakToe/TikTakToe.java
+++ src/TikTakToe/TikTakToe.java
@@ -1,7 +1,7 @@
 package TikTakToe;
 
 public class TikTakToe {
-    
+
 
 
 }
